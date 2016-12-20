3481626ad7ce8a768512d97a990ca06de199ca09
denversessink <denver@dsinternet.nl>
2016-12-20 15:12:11 +0100

    SQL file for creating the database

---
+++ 2016/database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `users` (
+  `id` INT(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `username` VARCHAR(30) NOT NULL,
+  `password` VARCHAR(30) NOT NULL
+);
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES (1, 'denver', '89&SD(F&^*789s7f)');
+INSERT INTO `users` (`id`, `username`, `password`) VALUES (2, 'wassink', 'sdf9878(&SDf98satd)');
+INSERT INTO `users` (`id`, `username`, `password`) VALUES (3, 'henk', '90as8d7f097YS)Df97t');
+INSERT INTO `users` (`id`, `username`, `password`) VALUES (4, 'sjonnie', 'jhsadf97sadyf8&DFYT');
